90ea459273196db8545eb5a005d0eae4868bbdc3
fazkal <fazelkalhory@gmail.com>
2025-01-13 22:27:10 +0330

    Defined url and view for redirect to another website.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     
     #path('cbv-index/', TemplateView.as_view(template_name='index.html',extra_context={"name":"fazel"})),
     path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
+    path('go-to-maktabkhooneh/<int:pk>',views.RedirectToMaktab.as_view(),name='redirect-to-maktabkhooneh'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render,get_object_or_404
+from django.views.generic.base import TemplateView,RedirectView
 from .models import Post
 # Create your views here.
 
@@ -11,3 +11,10 @@ class IndexView(TemplateView):
         context['name']='ali'
         context['posts']=Post.objects.all()
         return context
+    
+class RedirectToMaktab(RedirectView):
+    url='https://maktabkhooneh.com'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post=get_object_or_404(Post,pk=kwargs['pk'])
+        return super().get_redirect_url(*args, **kwargs)
