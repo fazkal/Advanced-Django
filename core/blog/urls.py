cf022acebf888aff76370cd5c9b849b6bc306b94
fazkal <fazelkalhory@gmail.com>
2025-01-14 12:54:58 +0330

    Defined url and view for PostDetailView.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
+app_name='blog'
+
 urlpatterns = [
     
     #path('cbv-index/', TemplateView.as_view(template_name='index.html',extra_context={"name":"fazel"})),
     path('cbv-index', views.IndexView.as_view(),name='cbv-index'),
     path('post/',views.PostList.as_view(),name='post-list'),
+    path('post/<int:pk>/',views.PostDetailView.as_view(),name='post-detail'),
     path('go-to-maktabkhooneh/<int:pk>',views.RedirectToMaktab.as_view(),name='redirect-to-maktabkhooneh'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic.base import TemplateView,RedirectView
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 # Create your views here.
 
@@ -30,3 +31,6 @@ class PostList(ListView):
     # def get_queryset(self):
         #posts=Post.objects.filter(status=True)
         #return posts
+
+class PostDetailView(DetailView):
+    model=Post
