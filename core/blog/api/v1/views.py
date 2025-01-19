97360ee33668278321df928a1c5744ec96eacf38
fazkal <fazelkalhory@gmail.com>
2025-01-19 14:44:48 +0330

    Defined status code for not found Versio 1 api blog posts.

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -9,6 +10,6 @@ def postList(request):
 
 @api_view()
 def postDetail(request,id):
-    post=Post.objects.get(pk=id)
+    post=get_object_or_404(Post,pk=id)
     serializer=PostSerializer(post)
     return Response(serializer.data)
