beb7fc6c48d60bbc35b219310313ca6c47922047
fazkal <fazelkalhory@gmail.com>
2025-01-21 15:54:14 +0330

    Function base api views and their urls for postList and postDetail was commented. Defined class base view inherted from APIView and url for PostList.

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -5,6 +5,7 @@ app_name='api-v1'
 
 urlpatterns = [
     
-    path('post/', views.postList,name='post-list'),
-    path('post/<int:id>',views.postDetail,name='post-detail'),
+    # path('post/', views.postList,name='post-list'),
+    # path('post/<int:id>',views.postDetail,name='post-detail'),
+    path('post/',views.PostList.as_view(),name='post-list'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
+""" function base api views: 
+"Getting a list of posts and creating new posts:"
 @api_view(["GET","POST"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def postList(request):
@@ -32,4 +35,21 @@ def postDetail(request,id):
         return Response(serializer.data)
     elif request.method=="DELETE":
         post.delete()
-        return Response({"Detail":"Item removed successfully"})
+        return Response({"Detail":"Item removed successfully"})"""
+
+""" Class base api views: """
+class PostList(APIView):
+    """Getting a list of posts and creating new posts"""
+
+    def get(self,request):
+        """Retrieving a list of posts """
+        posts=Post.objects.filter(status=True)
+        serializer=PostSerializer(posts,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        """Creating a post with providing data"""
+        serializer=PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
