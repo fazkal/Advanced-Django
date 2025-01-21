bf681adeb882adb05b3b194baab7383f0735c9a2
fazkal <fazelkalhory@gmail.com>
2025-01-22 01:12:02 +0330

    Added url for category on DefaultRouter.

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -7,6 +7,7 @@ app_name='api-v1'
 #Generating urls via DefaultRouter
 router=DefaultRouter()
 router.register('post',views.PostModelViewSet,basename='post')
+router.register('category',views.CategoryModelViewSet,basename='category')
 urlpatterns=router.urls
 
 # urlpatterns = [
