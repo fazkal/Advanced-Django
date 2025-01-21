f1f8d4ec3ce1523d961af7f3d179118d48924205
fazkal <fazelkalhory@gmail.com>
2025-01-22 00:51:25 +0330

    Defined CategorySerializer for category model.

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import Post
+from ...models import Post,Category
 
 '''
 class PostSerializer(serializers.Serializer):
@@ -12,3 +12,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model=Post
         fields=['id','author','title','content','category','status','created_date','published_date']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields=['id','name']
