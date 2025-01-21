0360c8293da8dca0b974b064c8530db9d80670eb
fazkal <fazelkalhory@gmail.com>
2025-01-22 01:01:40 +0330

    Defined DefaultRouter, and added PostModelViewSet on that. commented before urlpatterns.

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name='api-v1'
 
-urlpatterns = [
+#Generating urls via DefaultRouter
+router=DefaultRouter()
+router.register('post',views.PostModelViewSet,basename='post')
+urlpatterns=router.urls
+
+# urlpatterns = [
     
     # path('post/', views.postList,name='post-list'),
     # path('post/<int:id>',views.postDetail,name='post-detail'),
-    path('post/',views.PostList.as_view(),name='post-list'),
-    path('post/<int:pk>',views.PostDetail.as_view(),name='post-detail'),
-]
+    # path('post/',views.PostList.as_view(),name='post-list'),
+    # path('post/<int:pk>',views.PostDetail.as_view(),name='post-detail'),
+# ]
