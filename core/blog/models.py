4c5c185602d57625c1a6316cded4218c330d80c6
fazkal <fazelkalhory@gmail.com>
2025-01-24 17:40:10 +0330

    Defined __str__ class, snippet and absolute_api_url functions on Post model for some custom fields on PostSerializer class.

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your models here.
 
 #Getting user model objecet
@@ -16,6 +17,15 @@ class Post(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     updated_date=models.DateTimeField(auto_now=True)
     published_date=models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_snippet(self):
+        return self.content[0:5]
+    
+    def get_absolute_api_url(self):
+        return reverse('blog:api-v1:post-detail', kwargs={'pk':self.pk})
 
 class Category(models.Model):
     name=models.CharField(max_length=250)
