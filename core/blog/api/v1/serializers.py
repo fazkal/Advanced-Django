7c37683a458ab53188ad55e89170b4c871243b91
fazkal <fazelkalhory@gmail.com>
2025-01-24 17:52:59 +0330

    Defined some serializer fields for PostSerializer and wrote SerializerMethodField for create post's url value and display that according to the desired page.

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,14 +6,33 @@ class PostSerializer(serializers.Serializer):
     id=serializers.IntegerField()
     title=serializers.CharField(max_length=250)
 '''
-
-class PostSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model=Post
-        fields=['id','author','title','content','category','status','created_date','published_date']
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields=['id','name']
+
+class PostSerializer(serializers.ModelSerializer):
+    snippet=serializers.ReadOnlyField(source='get_snippet')
+    relative_url=serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url=serializers.SerializerMethodField(method_name='get_abs_url')
+    #category=serializers.SlugRelatedField(many=False,slug_field='name',queryset=Category.objects.all())
+
+    class Meta:
+        model=Post
+        fields=['id','author','title','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+
+    def get_abs_url(self,obj):
+        request=self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
+    def to_representation(self, instance):
+        request=self.context.get('request')
+        rep=super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet',None)
+            rep.pop('relative_url',None)
+            rep.pop('absolute_url',None)
+        else:
+            rep.pop('content',None)
+        rep['category']=CategorySerializer(instance.category).data
+        return rep
