b67e9557b2b595db379bbaa78de0c3e4856afb5e
fazkal <fazelkalhory@gmail.com>
2025-01-26 23:21:52 +0330

    Defined CustomAuthToken for accounts api and url for custom token authentication response .

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='api-v1'
 
 urlpatterns=[
-    path('registration/',views.RegistrationApiView.as_view(), name='registration')
+    path('registration/',views.RegistrationApiView.as_view(), name='registration'),
+    path('token/login/',views.CustomAuthToken.as_view(),name='token-login')
 ]
