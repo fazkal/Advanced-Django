d6c94ac6afb5b5e1851e10b166b8e3a1dae7b5d2
fazkal <fazelkalhory@gmail.com>
2025-02-01 00:44:15 +0330

    Defined urls for ActivationApiView and ActivationResendApiView on account api.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -12,8 +12,12 @@ urlpatterns=[
     # registration
     path('registration/',views.RegistrationApiView.as_view(), name='registration'),
 
-    # verification
+    # Activation and verification
+    path('activation/confirm/<str:token>',views.ActivationApiView.as_view(),name='activation'),
     path('send-mail/',views.TestEmailSend.as_view(),name='send-mail'),
+
+    # Resend Activation 
+    path('activation/resend/',views.ActivationResendApiView.as_view(),name='activation-resend'),
 
     # change password
     path('changed-password/', views.ChangePasswordApiView.as_view(),name='change-password'),
