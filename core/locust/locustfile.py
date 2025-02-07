d96663dc72f59838f6e7bfa74324e52ae9a0dc3d
fazkal <fazelkalhory@gmail.com>
2025-02-07 19:20:17 +0330

    Reformatted locustfile code.

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,13 +1,14 @@
-from locust import HttpUser,task
+from locust import HttpUser, task
+
 
 class QuickStartUser(HttpUser):
 
     def on_start(self):
-        response=self.client.post('/accounts/api/v1/jwt/create/',data={
-            "email":"sherifuller@example.net",
-            "password":"Test@123456"
-            }).json()
-        self.client.headers= {'Authorization': f"Bearer {response.get('access',None)}"}
+        response = self.client.post(
+            "/accounts/api/v1/jwt/create/",
+            data={"email": "sherifuller@example.net", "password": "Test@123456"},
+        ).json()
+        self.client.headers = {"Authorization": f"Bearer {response.get('access', None)}"}
 
     @task
     def post_list(self):
