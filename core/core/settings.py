f804faf6fcf5139ed458c4876c75f9ee764e2e6a
fazkal <fazelkalhory@gmail.com>
2025-02-07 15:30:05 +0330

    Added corsheaders on INSTALLED_APP, MIDDLEWARE and and CORS_ALLOWED_ORIGINS.

--- core/core/settings.py
+++ core/core/settings.py
@@ -51,16 +51,19 @@ INSTALLED_APPS = [
     "drf_yasg",
     "rest_framework_simplejwt",
     "mail_templated",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -178,3 +181,9 @@ EMAIL_HOST_USER ='test4mydev@gmail.com'
 EMAIL_HOST_PASSWORD ='App password'
 EMAIL_PORT =587
 """
+
+
+CORS_ALLOWED_ORIGINS = [
+
+        'http://127.0.0.1:5500'
+]
