f1a9d95a09af0d375fca11b8d24402e94fb908f7
fazkal <fazelkalhory@gmail.com>
2025-02-10 16:29:46 +0330

    Defined test_cache view on accounts views for test caching.

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,16 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .tasks import sendEmail
+from django.views.decorators.cache import cache_page
+import requests
 
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+# a view for test caching via redis:
+@cache_page(400)
+def test_cache(request):
+    response = requests.get("https://b2a3bf75-0773-48c8-be89-468d58a79c66.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
